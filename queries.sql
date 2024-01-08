28541d23b6e4b7635357ec560de9522418512982
Yahor <yahoryakubovich@gmail.com>
2024-01-08 13:07:12 +0100

    HOTFIX

--- queries.sql
+++ queries.sql
@@ -50,8 +50,7 @@ WITH ActorFilmCounts AS (
 
 SELECT actor_id, first_name, last_name, film_count
 FROM ActorFilmCounts
-WHERE film_count = (SELECT MAX(film_count) FROM ActorFilmCounts)
-ORDER BY film_count DESC, actor_id;
+WHERE film_count IN (SELECT film_count FROM ActorFilmCounts ORDER BY film_count DESC LIMIT 3);
 
 SELECT
     address.city_id,
@@ -63,3 +62,32 @@ JOIN customer ON address.address_id = customer.address_id
 JOIN city ON address.city_id = city.city_id
 GROUP BY address.city_id, city.city
 ORDER BY inactive_customers DESC;
+
+WITH FilmCategoryRentHours AS (
+    SELECT
+        category.name AS category_name, city.city,
+        address.city_id,
+        EXTRACT(EPOCH FROM (return_date - rental_date)) / 3600 AS hours_rented
+    FROM
+        rental
+    JOIN
+        inventory ON rental.inventory_id = inventory.inventory_id
+    JOIN
+        category ON inventory.film_id = category.category_id
+    JOIN
+        customer ON rental.customer_id = customer.customer_id
+    JOIN
+        address ON customer.address_id = address.address_id
+        JOIN city ON address.city_id = city.city_id
+)
+SELECT
+    category_name,
+    MAX(hours_rented) AS max_total_rent_hours
+FROM
+    FilmCategoryRentHours
+WHERE
+    (city_id IN (SELECT city_id FROM city WHERE city LIKE 'a%'))
+    OR
+    (city_id IN (SELECT city_id FROM city WHERE city LIKE '%-%'))
+GROUP BY
+    category_name;
